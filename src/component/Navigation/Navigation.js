6fe8be432cd2c49aec06caebec04d1a380821ad4
mdsymoon <mdsymoon11@gmail.com>
2021-11-10 19:55:07 -0800

    navBar complete

---
+++ src/component/Navigation/Navigation.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { AppBar, Box, Toolbar, Typography, Button } from "@mui/material";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+
+const Navigation = () => {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" sx={{backgroundColor:'#00293F'}}>
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            SCRIBD
+          </Typography>
+          <Button variant="outlined" color="inherit">
+            Sign In
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+};
+
+export default Navigation;
